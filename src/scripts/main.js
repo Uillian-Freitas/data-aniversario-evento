02d386e4b6af92e3420492cef6c2dfacc01bfe87
Uillian Freitas <freitasuillian@gmail.com>
2026-02-15 19:06:02 -0300

    projeto aniversario pronto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 import '../styles/main.scss';
 
-console.log("ola mundo");
+AOS.init();
+
+const dataDoEvento = new Date("Dec 26, 2026 21:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function (){
+const agora = new Date();
+const timeStampAtual = agora.getTime();
+
+const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+const diasEmMs = 1000 * 60 * 60 *24;
+const horaEmMs = 1000 * 60 * 60;
+const minutosEmMs = 1000 * 60;
+
+const dias = Math.floor(distanciaAteOEvento / diasEmMs);
+const horas = Math.floor((distanciaAteOEvento % diasEmMs) / horaEmMs);
+const minutos = Math.floor((distanciaAteOEvento % horaEmMs) / minutosEmMs);
+const segundos = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+document.getElementById("contador").innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`;
+
+if(distanciaAteOEvento < 0){
+    clearInterval(contaAsHoras);
+    document.getElementById('contador').innerHTML = 'O feitiço Acabou!'
+}
+
+
+}, 1000);
